997db27e7f6800472daa076954ce114b35e4c127
Harini R <hariniravichandran675@gmail.com>
2025-07-13 15:00:21 +0530

    DemoController.java

--- src/main/java/com/example/demo/Controller/DemoController.java
+++ src/main/java/com/example/demo/Controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Democontroller {
+public class DemoController {
 
 	@RequestMapping("/home")
 	public String home() {
